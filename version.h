f92b2f30c14bcb9ecbe547ff546f162d13231c77
JoachimDuquesne <joachim.duquesne@gmail.com>
2017-10-23 15:46:47 +0000

    Ok now everything is included for compatibility

---
+++ version.h
@@ -0,0 +1,7 @@
+#ifndef __VERSION_H__
+#define __VERSION_H__
+#define VERSION_DATE "Mon, 23 Oct 2017 15:29:25 +0000"
+#define VERSION_HASH "dc38ab0"
+#define VERSION_BRANCH "MQTT"
+#define VERSION_REPO "https://github.com/JoachimDuquesne/omxplayer2.git"
+#endif
